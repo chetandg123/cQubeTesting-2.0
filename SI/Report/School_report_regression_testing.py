2911967e339f7e9eb1f9f5ff0b430c3412a1471a
chetanDG <chetan.goudar@tibilsolutions.com>
2020-07-07 22:01:29 +0530

    "final update"

--- SI/Report/School_report_regression_testing.py
+++ SI/Report/School_report_regression_testing.py
@@ -14,22 +14,22 @@ from SI.Report.check_homebtn import home_button
 from SI.Report.check_sc_scattor_blockwise_records import school_blockwise
 from SI.Report.check_sc_scattor_clusterwise_records import Test_schoolwise
 from SI.Report.check_sc_scattor_districtwise_records import test_districtwise
+
 from SI.Report.check_table_data_metrics import download_report
 from SI.Report.check_table_present_on_schoolinfra import check_with_table
 from SI.Report.check_tabledata_by_selecting_districts import districtwise_tabledata
-
 from SI.Report.check_with_hyperlink import Hyperlink
 from SI.Report.click_on_Report_from_scinfra import check_schoolinfra_report
+
 from SI.Report.click_on_district_and_click_download import download_districtwise
 from SI.Report.click_on_district_block_cluster_home import check_home
 from SI.Report.click_on_table_and_check_with_orderof_values import check_order_of_tabledata
-
 from SI.Report.download_blockwise_csv import donwload_blockwise_csv
 from SI.Report.download_districtwise_csv import download_district_wise_csv
+
 from SI.Report.navigate_to_SI_report import si_report
 from SI.Report.navigate_to_dashboard import check_dashboard
 from SI.Report.navigate_to_schoolinfra_and_click_on_logout import schoolinfra_logout
-
 from reuse_func import GetData
 
 
